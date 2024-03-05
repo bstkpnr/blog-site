e0d5edcdb2f5a508a94be3c74d85402a047091d5
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2024-03-05 12:45:39 +0300

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
       <h1 className="title">Beste Tokpınar</h1>
       <ul className="navList">
         <li className="navItem">
-        <Link to="/" style={{textDecoration:'none',color:'#9f2cbf'}}>Anasayfa</Link>
+        <Link to="/" style={{textDecoration:'none',color:'#9f2cbf'}}>Home</Link>
         </li>
         <li className="navItem">
         <Link to="/blog" style={{textDecoration:'none',color:'#9f2cbf'}}>Blog</Link>
